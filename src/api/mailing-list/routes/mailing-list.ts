d110fb7db7a8a6056c55d2f7be5010f63dd2f5de
Sebastian Fürndraht <sebastian.fuerndraht@gmail.com>
2023-04-30 17:03:11 +0200

    added mailerlist property to users collection

--- src/api/mailing-list/routes/mailing-list.ts
+++
@@ -1,7 +0,0 @@
-/**
- * mailing-list router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::mailing-list.mailing-list');
--- src/api/mailing-list/services/mailing-list.ts
+++
@@ -1,7 +0,0 @@
-/**
- * mailing-list service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::mailing-list.mailing-list');
