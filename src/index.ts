f88249b8b75aafd19cbc126bb88e283e33cd8644
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2023-05-29 10:50:05 +0200

    add attribution

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,20 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({strapi}) {
+    const extensionService = strapi.plugin("graphql").service("extension");
+    extensionService.use(({nexus}) => ({
+      types: [
+        nexus.extendType({
+          type: "UsersPermissionsRegisterInput",
+          definition(t) {
+            // here define fields you need
+            t.string("attribution");
+          },
+        }),
+      ]
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -14,5 +27,6 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) {
+  },
 };
